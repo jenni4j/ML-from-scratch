3f202173bb14bc66c242a8b9ed9eb30995a55e26
jenni4j <jenni4j@gmail.com>
2017-09-22 23:29:41 -0400

    dating

--- knn/knn.py
+++ knn/knn.py
@@ -35,6 +35,21 @@ def classify0(inX, dataSet, labels, k):
     sortedClassCount = sorted(classCount, key=classCount.__getitem__,reverse=True)
     return sortedClassCount[0]
 
+def file2matrix(filename):
+    file = open(filename)
+    num_lines = len(file.readlines())
+    return_matrix = zeros((num_lines,3))
+    class_label_vector = []
+    index = 0
+    for line in file.readlines():
+        line = line.strip()
+        list_from_line = line.split('\t')
+        return_matrix[index,:] = list_from_line[0:3]
+        class_label_vector.append(int(list_from_line[-1]))
+        index += 1
+    return return_matrix, class_label_vector
+    
+
 if __name__ == "__main__":
     group, labels = createDataSet()
     results = classify0([0,0],group,labels,3)
