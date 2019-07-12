05eec2d0d6851821d8b7ab4c88ad7d4958dcc527
jenni4j <jenni4j@gmail.com>
2019-07-11 23:47:30 -0700

    creating final tree and plot

--- trees/trees.py
+++ trees/trees.py
@@ -1,4 +1,6 @@
 from math import log
+import operator
+import matplotlib.pyplot as plt
 
 def createDataSet():
     dataSet = [[1,1,'yes'],[1,1,'yes'],[1,0,'no'],[0,1,'no'],[0,1,'no']]
@@ -53,4 +55,47 @@ def chooseBestFeatureToSplit(dataSet):
             bestFeature = i
         return bestFeature
 
+#helper function to return the class that occurs with the greatest frequency
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys(): 
+            classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCout.iteritems(),key=operator.itemgetter(1),reverse=True)
+    return sortedClassCount[0][0]
 
+
+#finally function to build the tree
+def createTree(dataSet,labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel:{}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet,bestFeat,value),subLabels)
+    return myTree
+
+#lets try draw this tree!
+decisionNode = dict(boxstyle = "sawtooth", fc="0.8")
+leafNode = dict(boxstyle = "round4", fc="0.8")
+arrow_args = dict(arrowstyle="<-")
+
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+    createPlot.ax1.annotate(nodeTxt, xy=parentPt, xycoords="axes fraction", xytext=centerPt, textcoords="axes fraction", va="center", ha="center", bbox=nodeType, arrowprops=arrow_args)
+
+def createPlot():
+    fig = plt.figre(1, facecolor="white")
+    fig.clf()
+    createPlot.ax1 = plt.subplot(111, frameon=False)
+    plotNode("a decision node", (0.5,0.1), (0.1,0.5), decisionNode)
+    plotNode("a leaf node", (0.8,0.1), (0.3,0.8), leafNode)
+    plt.show()
