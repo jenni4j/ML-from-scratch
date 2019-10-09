c5cda1f65505a27eb533f2a5fad5e88f9691e1de
jenni4j <jenni4j@gmail.com>
2019-10-08 21:51:21 -0700

    fixed data loader

--- naivebayes/bayes.py
+++ naivebayes/bayes.py
@@ -1,18 +1,13 @@
 def loadDataSet():
-postingList=[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
-                         ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
-                         ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
-                         ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
-                         ['mr', 'licks', 'ate', 'my', 'steak', 'how','to', 'stop', 'him'],
-                         ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
-classVec = [0,1,0,1,0,1]
-return postingList,classVec
+    postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],['stop', 'posting', 'stupid', 'worthless', 'garbage'],['mr', 'licks', 'ate', 'my', 'steak', 'how','to', 'stop', 'him'],['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+    classVec = [0,1,0,1,0,1]
+    return postingList,classVec
 
 def createVocabList(dataSet):
     vocabSet = set([])
     for document in dataSet:
         vocabSet = vocabSet | set(document)
-    return list(vocabList, inputSet):
+    return list(vocabSet)
 
 def setOfWords2Vec(vocabList, inputSet):
     returnVec = [0]*len(vocabList)
@@ -20,5 +15,7 @@ def setOfWords2Vec(vocabList, inputSet):
         if word in vocabList:
             returnVec[vocabList.index(word)] = 1
         else:
-            print "the word: %s is not in my vocabulary!" % word
+            print("the word: %s is not in my vocabulary!" % word)
     return returnVec
+
+
